1a0f42741dce01008393956512bb92acbf8743fc
ASM072 <ahmedsaadmalik8@gmail.com>
2024-01-25 16:52:15 +0400

    styling and bug fixes

--- src/App.js
+++ src/App.js
@@ -1,73 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './App.css';
+import Calculator from './calculator';
 
-const Calculator = () => {
-  const [inputRow, setinputRow] = useState([
-    { id: 0, value: 0, sign: '+', enabled: true },
-  ] );
-  
-  const handleEnable = () =>
-  {
-    
-  }
-  
-  const handleRemove = () =>
-  {
-        setinputRow((previnputRow) => previnputRow.filter((row) => row.id !== id));
 
-  }
-
-  const handleAdd = () =>
-  {
-    setinputRow((previnputRow) => [
-      ...previnputRow,
-      { id: previnputRow.length, value: 0, sign: '+', enabled: true },
-    ]);
-  }
-
-  const handleValue = () =>
-  {
-    setinputRow((previnputRow) =>
-      previnputRow.map((row) => (row.id === id ? { ...row, value } : row))
-    );
-  };
-
-  const calculateTotal = () =>
-  {
-    
-  };
-
-  const signHandler = () =>
-  {
-    setinputRow((previnputRow) =>
-      previnputRow.map((row) => (row.id === id ? { ...row, sign } : row))
-    );
-  }
-return (
-  <div>
-  <button onClick={handleAdd}>Add Row</button><br></br>
-    { inputRow.map( ( row ) => (
-      <div key={row.id}>
-        <select
-          value={(0)}
-          onChange={(e) => signHandler()}>
-          <option value="+">+</option>
-          <option value="-">-</option>
-        </select>
-        <input
-          type="number"
-          value={(row.value)}
-          onChange={(e) => handleValue()}/>
-        <button onClick={() => handleEnable()}>
-          Disable
-        </button>
-        <button onClick={() => handleRemove()}>Remove</button>
-        </div>
-      ))}
-      <div>
-        <strong>Total:</strong> {calculateTotal()}
-      </div>
-  </div>
+const App =() => {
+    return (
+      <Calculator/>   
   );
-};
+}
 
-export default Calculator;
+export default App;
---
+++ src/calculator.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { evaluate } from 'mathjs';
+import './styles.scss'
+const Calculator = () => {
+  const [inputRow, setinputRow] = useState([
+    { id: 0, value: 0, sign: '+', enabled: true },
+  ] );
+  
+  const handleEnable = (id, ) =>
+  {
+    setinputRow((previnputRow) =>
+      previnputRow.map((row) => (row.id === id ? { ...row, enabled: !row.enabled } : row))
+    );
+  }
+  
+  const handleRemove = (id) =>
+  {
+    setinputRow((previnputRow) => previnputRow.filter((row) => row.id !== id));
+
+  }
+
+  const handleAdd = () =>
+  {
+    setinputRow((previnputRow) => [
+      ...previnputRow,
+      { id: previnputRow.length, value: 0, sign: '+', enabled: true },
+    ]);
+  }
+
+  const handleValue = (id, value) =>
+  {
+    setinputRow((previnputRow) =>
+      previnputRow.map((row) => (row.id === id ? { ...row, value } : row))
+    );
+  };
+
+  const calculateTotal = () =>
+  {
+    const enabledRow = inputRow.filter((row) => row.enabled);
+    const addition = enabledRow
+      .map((row) => `${row.sign}${row.value}`)
+      .join('');
+    return evaluate(addition);
+  };
+
+  const signHandler = (id, sign) =>
+  {
+    setinputRow((previnputRow) =>
+      previnputRow.map((row) => (row.id === id ? { ...row, sign } : row))
+    );
+  }
+return (
+  <div className="calculator" >
+    <h1>CoolShop Test</h1>
+  <button onClick={handleAdd}>Add Row</button><br></br>
+    { inputRow.map( ( row ) => (
+      <div key={row.id}>
+        <select
+          value={(row.sign)}
+          onChange={(e) => signHandler(row.id, e.target.value)}>
+          <option value="+">+</option>
+          <option value="-">-</option>
+        </select>
+        <input
+          type="number"
+          value={(row.value)}
+          onChange={ ( e ) => handleValue( row.id, e.target.value ) }
+          disabled={!row.enabled}
+        />
+        <button onClick={() => handleEnable(row.id)}>
+          {row.enabled?'Disable':'Enable'}
+        </button>
+        <button onClick={() => handleRemove(row.id)}>Remove</button>
+        </div>
+      ))}
+      <div>
+        <strong>Total:</strong> {calculateTotal()}
+      </div>
+  </div>
+  );
+};
+
+export default Calculator;
